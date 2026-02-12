dfe9550b278a4b4cb4a0fefbdbbcd1a99162a7e7
pawan0456 <pawankumarrana76@gmail.com>
2026-02-12 16:58:46 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_namespace_packages,setup
+from typing import List
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    this function will return the list of 
+    
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Pawan',
+author_email='pawankumarrana76@gmail.com',
+packages=find_namespace_packages(),
+install_requires=get_requirements('requirements.txt')
+)
